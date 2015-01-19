453514e4f3788b886f1530ad938370f24ae10e72
Shannon Zhao <shannon.zhaosl@gmail.com>
2015-01-19 23:01:21 +0800

    Setting up an OnClickListener on a Button
    Setting up an OnClickListener on a Button.
    
    Signed-off-by: Shannon Zhao <shannon.zhaosl@gmail.com>
    M#L&

--- app/src/main/java/com/example/shannon/myfirstapp/MainActivity.java
+++ app/src/main/java/com/example/shannon/myfirstapp/MainActivity.java
@@ -1,17 +1,45 @@
 package com.example.shannon.myfirstapp;
 
+import android.graphics.Color;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.app.Activity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.LinearLayout;
 
 
 public class MainActivity extends ActionBarActivity {
 
+    LinearLayout background;
+    Button btnGreen;
+    Button btnBlue;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.linear_layout);
+
+        background = (LinearLayout) findViewById(R.id.background);
+        btnGreen = (Button) findViewById(R.id.btnGreen);
+        btnBlue = (Button) findViewById(R.id.btnBlue);
+
+        btnGreen.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //click button code here
+                background.setBackgroundColor(Color.parseColor("#00ff00"));
+            }
+        });
+
+        btnBlue.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                background.setBackgroundColor(Color.parseColor("#006699"));
+            }
+        });
     }
 
 
